7d2012c463afaad1685c34d59fdb32a032683635
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-10-19 21:55:34 +0900

    TAP-2, paging3のgradle追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,23 @@ android {
 
 dependencies {
 
+    val paging_version = "3.3.2"
+
+    implementation("androidx.paging:paging-runtime-ktx:$paging_version")
+
+    // alternatively - without Android dependencies for tests
+    testImplementation("androidx.paging:paging-common-ktx:$paging_version")
+
+    // optional - RxJava2 support
+    implementation("androidx.paging:paging-rxjava2-ktx:$paging_version")
+
+    // optional - RxJava3 support
+    implementation("androidx.paging:paging-rxjava3:$paging_version")
+
+    // optional - Guava ListenableFuture support
+    implementation("androidx.paging:paging-guava:$paging_version")
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.leanback)
     implementation(libs.glide)
--- app/src/main/java/com/example/tvapplicationpaging3/MainActivity.kt
+++ app/src/main/java/com/example/tvapplicationpaging3/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.tvapplicationpaging3
 
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
 import androidx.fragment.app.FragmentActivity
 
 /**
@@ -12,9 +14,14 @@ class MainActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
+            supportFragmentManager.beginTransaction()
                 .replace(R.id.main_browse_fragment, MainFragment())
                 .commitNow()
         }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        Log.d("honda","")
+        return super.onKeyDown(keyCode, event)
     }
 }
