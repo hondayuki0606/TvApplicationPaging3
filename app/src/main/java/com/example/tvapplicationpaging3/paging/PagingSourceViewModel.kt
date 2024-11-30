051adeae8f9de30365cfe1219e10bf4989074482
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-11-30 23:52:11 +0900

    TAP-35, refreshするためのアイテムを特定するための方法を調査2

--- app/src/main/java/com/example/tvapplicationpaging3/paging/PagingSourceViewModel.kt
+++ app/src/main/java/com/example/tvapplicationpaging3/paging/PagingSourceViewModel.kt
@@ -1,11 +1,10 @@
 package com.example.tvapplicationpaging3.paging
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import androidx.paging.insertSeparators
+import androidx.paging.filter
 import androidx.paging.map
 import com.example.tvapplicationpaging3.Movie
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,7 +12,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -32,13 +31,13 @@ class PagingSourceViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(UiState())
     val uiState: StateFlow<UiState> = _uiState
     private val intList = Array(100) { it }
-    val startPosition = 80
+    val startPosition = 0
     fun load() {
         // 最初に仮データを設定
         loadFakeData()
 
         // 実際のデータを後から取得
-        fetchMovies()
+        fetchMovies2()
     }
 
     private fun loadFakeData() {
@@ -62,20 +61,57 @@ class PagingSourceViewModel @Inject constructor(
 //        }
     }
 
-    private fun fetchMovies() {
+
+    //    private fun fetchMovies() {
+//        viewModelScope.launch {
+//            // 実際のデータを取得（例えばAPIから）
+//            moviesRepository.getMovies(startPosition = startPosition, intList = intList)
+//                .collectLatest { pagingData ->
+//                    // 実際のデータで更新
+//                    _uiState.update {
+//                        it.copy(
+//                            pagingDataFlow = pagingData,
+//                            isLoading = true,
+//                            errorMessage = null
+//                        )
+//                    }
+//                }
+//        }
+//    }
+    private val _localDataList = MutableStateFlow(listOf<Movie>())
+    fun fetchMovies2() {
         viewModelScope.launch {
             // 実際のデータを取得（例えばAPIから）
-            moviesRepository.getMovies(startPosition = startPosition, intList = intList)
-                .collectLatest { pagingData ->
-                    // 実際のデータで更新
-                    _uiState.update {
-                        it.copy(
-                            pagingDataFlow = pagingData,
-                            isLoading = true,
-                            errorMessage = null
-                        )
-                    }
+            moviesRepository.getMovies2(startPosition = startPosition, intList = intList).cachedIn(
+                viewModelScope
+            ).combine(_localDataList) { paging, local ->
+                paging.map {
+                    if (it.id == local.firstOrNull()?.id) local
+                    else it
                 }
+            }
+//
+
+            
+        }
+    }
+
+    fun updateItem(item: Movie) {
+        val updateItem = Movie(0,"","","","","","")
+       val list = _uiState.value.pagingDataFlow
+        val list2 = _uiState.value.pagingDataFlow.map {  }
+        val newList = _localDataList.value.filterNot { it.id == updateItem.id }
+//        _localDataList.update {
+//            i
+//        }
+        _localDataList.value = newList
+//        _uiState.value.pagingDataFlow.filter { it.id ==  }
+        _uiState.update {
+            it.copy(
+                pagingDataFlow = list,
+                isLoading = true,
+                errorMessage = null
+            )
         }
     }
 
