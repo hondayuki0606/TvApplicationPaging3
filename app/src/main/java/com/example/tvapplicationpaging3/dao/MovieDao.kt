2329371c65be35b14c02b61a6bd4242a55a3a070
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-12-11 21:24:55 +0900

    TAP-48, MovieDao.ktの不要なimport削除

--- app/src/main/java/com/example/tvapplicationpaging3/dao/MovieDao.kt
+++ app/src/main/java/com/example/tvapplicationpaging3/dao/MovieDao.kt
@@ -3,7 +3,6 @@ package com.example.tvapplicationpaging3.dao
 
 import androidx.paging.PagingSource
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
